8d9b491315a3b13f6ee07cd4eb535187eb529a95
Sadra <sadrajabbarisani@gmail.com>
2024-07-18 13:54:43 +0330

    create MineTile class and inherits from JButton

---
+++ src/forms/MineTile.java
@@ -0,0 +1,12 @@
+package forms;
+
+import javax.swing.*;
+
+public class MineTile extends JButton{
+    int row, col;
+    
+    public MineTile(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+}
